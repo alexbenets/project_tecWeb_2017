4bd05f098aeb96fe6e7df95edfc5e480e1af88e0
alexbenets <alexbenets@gmail.com>
2017-02-03 19:54:39 +0100

    Completato WebService

---
+++ php/Web service/config_server.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Server Directories configuration variables
+ */
+define("PROJECT_DIR", "Ireferee");
+define("MEDIA_DIR", "media");
+define("TEMP_DIR", "temp");
+define("ATTACHMENT_DIR", "attachment");
+define("REPORT_DIR", "report");
+define("WALL_DIR", "wall");
+define("SIGNATURE_DIR", "signature");
+define("PROFILE_DIR", "profile");
+
+
+?>
